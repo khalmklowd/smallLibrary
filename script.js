07f424225060e7887c0b8b6069ad1be2aa6b3d70
khalmklowd <kylewmcleod@hotmail.com>
2021-02-09 08:27:47 -0500

    Html, css, and js for smallLibrary

---
+++ script.js
@@ -0,0 +1,41 @@
+const theHobbit =  new book("The Hobbit", "Tolkien", 295, 250, "Not finished.");
+ 
+var myLibrary = [theHobbit];
+
+function book(title, author, pages, pagesRead, notes) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.pagesRead = pagesRead;
+    this.notes = notes;
+    this.info = `${title} by ${author}, has ${pages} pages and you have ${pages-pagesRead} pages left to read. ${notes}`;
+  } 
+
+function addBook() {
+  tit = prompt("Title?");
+  auth = prompt("Author?");
+  pages = prompt("Pages?");
+  readIt = prompt("How many pages have you read?")
+  notes = prompt("Any notes to add?")
+  myLibrary = new book(tit, auth, pages, readIt, notes);
+  putToScreen(myLibrary);
+}
+
+function putToScreen(books) {
+    var th = document.createElement("TH");
+    var tr = document.createElement("TR");
+    book.prototype.toString = function() { 
+      return ''+this.title;
+    }
+    th.innerHTML = myLibrary.toString();
+
+    book.prototype.toString = function() { 
+      return ''+this.info;
+    }
+    tr.innerHTML = myLibrary.toString();
+    
+    document.getElementById("tbody").appendChild(th);
+    document.getElementById("tbody").appendChild(tr);
+  }
+
+
